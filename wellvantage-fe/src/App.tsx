bc0ef0b955085d77190d7a4c25da35b29f045d0c
Prashant Halaki <prashanthalaki143@gmail.com>
2025-10-31 11:46:24 +0530

    fix: gh-page

--- wellvantage-fe/src/App.tsx
+++ wellvantage-fe/src/App.tsx
@@ -20,7 +20,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename='/wellvantage/wellvantage-fe'>
+      <BrowserRouter>
         <AuthProvider>
           <Routes>
             <Route path='/' element={<LandingScreen />} />
